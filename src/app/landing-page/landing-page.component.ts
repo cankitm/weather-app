71f94f925643d4b41e33635f1f05f4c39c66d068
Ankit Mishra <ankitmishra@Ankit-MacBook-Pro.local>
2020-07-05 14:03:00 +0530

    Completed basic setup

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { LandingPageService } from './landing-page.service';
 
@@ -7,13 +7,26 @@ import { LandingPageService } from './landing-page.service';
   selector: "landing-page",
   templateUrl: "./landing-page.component.html",
   styleUrls: ["./landing-page.component.css"],
-  providers: [LandingPageService]
+  providers: [LandingPageService],
+  encapsulation: ViewEncapsulation.None
+
 })
 
 export class LandingPageComponent implements OnInit {
   weather_data: any;
+  loader: boolean;
+  current_temp = {
+    temp: null,
+    format: "C"
+  };
+  time = {
+    sunrise: null,
+    sunset: null
+  }
   constructor(private httpClient: HttpClient, private landingPageService: LandingPageService) {
     this.weather_data = {};
+    this.loader = true;
+
   }
 
   ngOnInit() {
@@ -26,10 +39,33 @@ export class LandingPageComponent implements OnInit {
     })
   }
 
+  changeFormat() {
+    if (this.current_temp.format === 'C') {
+      this.current_temp.temp = Math.round(this.weather_data.main.temp * (9 / 5) + 32);
+      this.current_temp.format = 'F';
+    }
+    else if (this.current_temp.format === 'F') {
+      this.current_temp.temp = this.weather_data.main.temp;
+      this.current_temp.format = 'C';
+    }
+  }
+
   getWeatherData(lat, lon) {
     this.landingPageService.getWeather(lat, lon).subscribe((responseData) => {
+      this.loader = false;
       this.weather_data = responseData;
+      this.current_temp.temp = this.weather_data.main.temp;
+      // this.time.sunrise = this.getReadableDate(this.weather_data.sys.sunrise)
+
     })
   }
+
+  // getReadableDate(currMill) {
+  //   let date = new Date(currMill);
+  //   console.log(date.toTimeString())
+  //   return (date.toString())
+  // }
+
+
 
 }
